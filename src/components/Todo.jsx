988adc3a41a0a3e9c37e0089039c1b7157cc9dff
relative-rene <rene.arellano415@gmail.com>
2024-04-17 17:13:16 -0700

    adds UTC time to todo due date

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
 
-function Todo({ description, due_date_and_time, is_done, onDelete, onUpdate, onGroup, todo_id, isEditing, isGrouping }) {
+function Todo({ description, due_date_and_time, is_done, onDelete, onUpdate, todo_id, isEditing }) {
     const [editDescription, setEditDescription] = useState(description);
-    const [day, month, date, year, time] = new Date((due_date_and_time).toLocalString()).toString().split(' ')// ['Fri', 'Mar', '22', '2024', '04:30:00', 'GMT-0700', '(Pacific', 'Daylight', 'Time)']
-    const am_pm = 12 >= Number(time.split(':')[0]) ? 'pm' : 'am';
-    const dueDate = `${day} ${month}/${date}/${year} ${time.split(':').slice(0, 2).join(':')} ${am_pm}`;
+    const iso = new Date(due_date_and_time).toLocaleString();
+    const [shortDate, time] = iso.split(',');
+    const [weekday, month, date, year] = new Date(shortDate).toString().split(' ')
+    const dueDate = `${weekday} ${month} ${date}, ${year} ${time}`;
 
     return (
-        <li key={'task-' + todo_id} onClick={() => onGroup(todo_id)} className={is_done ? "Todo checked" : "Todo"}>
+        <li key={'task-' + todo_id} className={is_done ? "Todo checked" : "Todo"}>
             <input type="checkbox" onChange={() => onUpdate(todo_id)} checked={is_done} />
-            {isEditing?
-            <>
-                <i onClick={() => onDelete(todo_id)} className="fa-solid fa-trash-can" />
-                <input value={editDescription} type="text" onChange={(e) => setEditDescription(e.target.value)} />
-            </>: `${description}`}
-            <br/>{!isEditing && dueDate}
+            {isEditing ?
+                <>
+                    <i onClick={() => onDelete(todo_id)} className="fa-solid fa-trash-can" />
+                    <input value={editDescription} type="text" onChange={(e) => setEditDescription(e.target.value)} />
+                </> : `${description}`}
+            <br />{!isEditing && dueDate}
         </li>
     );
 }
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -4,26 +4,21 @@ import Button from './UI/Button';
 import { useAuth } from '../hooks/useAuth'
 
 const TodoApp = ({ list }) => {
-    //const { user } = useAuth() TODO uncomment once ready, its commented out because I want all my TODO not linked to login for the moment
-    const [profile_id, setProfileID] = useState('650a38c328bf0c645f4d8f66');// TODO replace profile_id with user._id
+    const { user } = useAuth();
     const [todoList, setList] = useState(list);
     const [description, setDescription] = useState('');
     const [dueDateAndTime, setDueDate] = useState('');
-    const [groupedTask, setGroup] = useState([]);
     const [onEditing, setEditingMode] = useState(false);
-    const [onGrouping, setGroupingMode] = useState(false);
     const minDateValue = new Date().toISOString().slice(0, 16);
     const isReady = description && dueDateAndTime.length;
 
-    const todos = todoList.map((entry, idx) => {
+    const todos = todoList.map((entry) => {
         return <Todo
             isEditing={onEditing}
-            isGrouping={onGrouping}
             onUpdate={onUpdateHandler}
             onDelete={onDeleteHandler}
-            onGroup={onGroupingHandler}
             todo_id={entry._id}
-            key={"todo-" + idx} {...entry} />
+            key={"todo-" + entry._id} {...entry} />
     });
 
     useEffect(() => {
@@ -31,8 +26,7 @@ const TodoApp = ({ list }) => {
     }, []);
 
     async function getTodoList() {
-        // return fetch(`${process.env.REACT_APP_SERVER}api/profiles/${user._id}/todos/read`)
-        return fetch(`${process.env.REACT_APP_SERVER}/api/profiles/${profile_id}/todos/read`)
+        return fetch(`${process.env.REACT_APP_SERVER}/api/profiles/${user._id}/todos/read`)
             .then(res => res.json())
             .then(data => {
                 let sortByDueDate = data.sort((a, b) => new Date(a.due_date_and_time) - new Date(b.due_date_and_time));
@@ -42,46 +36,26 @@ const TodoApp = ({ list }) => {
     }
 
     async function onAddHandler() {
-        const saveTodo = { description, isDone: false, dueDateAndTime }
+        const saveTodo = { description, isDone: false, dueDateAndTime: new Date(dueDateAndTime).toUTCString() }
         if (description.length !== 0) {
-            // const response = await fetch(`${process.env.REACT_APP_SERVER}api/profiles/${user._id}/todo/create`,
-            const response = await fetch(`${process.env.REACT_APP_SERVER}/api/profiles/${profile_id}/todo/create`,
+            const response = await fetch(`${process.env.REACT_APP_SERVER}/api/profiles/${user._id}/todo/create`,
+                // const response = await fetch(`${process.env.REACT_APP_SERVER}/api/profiles/${profile_id}/todo/create`,
                 { method: 'post', body: JSON.stringify(saveTodo), headers: { "Content-Type": "application/json" } })
             const data = await response.json();
             setDescription('') && setDueDate('');
-
             data && getTodoList();
         }
     };
 
     async function onDeleteHandler(todo_id) {
-        const response = await fetch(`${process.env.REACT_APP_SERVER}/api/profiles/${profile_id}/todos/${todo_id}/delete`, { method: 'DELETE' });
-        // const response = await fetch(`${process.env.REACT_APP_SERVER}api/profiles/${user._id}/todos/${todo_id}/delete`, { method: 'DELETE' });
+        const response = await fetch(`${process.env.REACT_APP_SERVER}/api/profiles/${user._id}/todos/${todo_id}/delete`, { method: 'DELETE' });
         const data = await response.json();
         data && getTodoList();
     };
 
-    async function onHandleGroupClick(e) {
-        console.log(e)
-        setGroupingMode(!onGrouping)
-        setGroup([])
-        // Create a grouping api
-        // What's the goal of group
-        // What it needs to accomplish
-        // create group user interface
-        // add group route, model and controller(maybe);
-    }
-    async function onGroupingHandler(id) {
-        if (onGrouping) {
-            setGroup([...groupedTask, id])
-        }
-        console.log(groupedTask);
-    }
-
     async function onUpdateHandler(todo_id) {
         const newTodo = todoList.filter(todo => todo._id === todo_id).map(todo => Object.assign({}, { ...todo, is_done: !todo.is_done }));
-        // const response = await fetch(`${process.env.REACT_APP_SERVER}api/profiles/${user._id}/todos/${todo_id}/updateOne`, { method: 'PUT', body: JSON.stringify(newTodo), headers: { "Content-Type": "application/json" } });
-        const response = await fetch(`${process.env.REACT_APP_SERVER}/api/profiles/${profile_id}/todos/${todo_id}/updateOne`, { method: 'PUT', body: JSON.stringify(newTodo), headers: { "Content-Type": "application/json" } });
+        const response = await fetch(`${process.env.REACT_APP_SERVER}/api/profiles/${user._id}/todos/${todo_id}/updateOne`, { method: 'PUT', body: JSON.stringify(newTodo), headers: { "Content-Type": "application/json" } });
         const data = await response.json();
         data && getTodoList();
     }
@@ -89,18 +63,14 @@ const TodoApp = ({ list }) => {
     return (
         <div className="TodoApp">
             <div className="AddTodo">
-                <i onClick={() => setEditingMode(!onEditing)} title="Edit mode: remove task or edit description" className="fa-solid fa-pen" />
-                
-                {/*TODO consider adding grouping funcitonality.  
-                    <i onClick={onHandleGroupClick} title="Group mode: click before selecting which task to group" className="fa-solid fa-cubes-stacked" /> 
-                */}
+                <i onClick={() => setEditingMode(!onEditing)} title="Edit mode: remove task or edit description" className="fa-solid fa-pen fa-lg" />
                 <input value={description} type="text" onChange={(e) => setDescription(e.target.value)} />
                 <Button styleName="btn-todo" isDisabled={!isReady} handleClick={onAddHandler} >+</Button>
 
                 {description &&
                     <label className="datetimeInput">
                         Complete By
-                        <input value={dueDateAndTime} min={minDateValue} onChange={(e) => setDueDate((e.target.value).toUTCString())} type="datetime-local" />
+                        <input value={dueDateAndTime} min={minDateValue} onChange={(e) => setDueDate((e.target.value))} type="datetime-local" />
                     </label>}
             </div>
             <ul className="TodoList">
