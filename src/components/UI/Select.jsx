1da3b10711909b1f2a26330ad8632afd1e036583
relative-rene <rene.arellano415@gmail.com>
2024-05-16 14:46:45 -0700

    update requiered validation

--- src/components/UI/Select.jsx
+++ src/components/UI/Select.jsx
@@ -1,11 +1,16 @@
 
 
-const Select = ({ updateForm, selectedVal, nameForId, options, label }) => {
+const Select = ({ updateForm, selectedVal, nameForId, options, label,validations, children }) => {
 
     return (
         <div className="SelectComponent">
-            <label htmlFor={nameForId}>{label}</label>
-            <select value={selectedVal} onChange={(e) => updateForm(nameForId, e.target.value)} name={nameForId} id={nameForId}>
+            <label htmlFor={nameForId}>{label}{children}</label>
+            <select 
+                required={validations ? validations.required : false} 
+                value={selectedVal} 
+                onChange={(e) => updateForm(nameForId, e.target.value)} 
+                name={nameForId} 
+                id={nameForId}>
                 <option value="">Choose an option</option>
                 {options}
             </select>
