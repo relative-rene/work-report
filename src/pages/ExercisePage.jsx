758f12593594edd699d0641fae89b5f2b25c79e4
relative-rene <rene.arellano415@gmail.com>
2024-05-28 21:35:20 -0700

    add exercise search

--- src/pages/ExercisePage.jsx
+++ src/pages/ExercisePage.jsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react';
+import { lazy, useState, useMemo } from 'react';
 import AddExercise from '../components/AddExercise';
 import LoadingSpinner from '../components/UI/LoadingSpinner';
 import { EXERCISE_KEY_LABELS } from '../data/constants';
@@ -8,13 +8,30 @@ const Table = lazy(() => import('../components/UI/Table'));
 
 const ExercisePage = () => {
     let { exercises } = useData();
+    const [query, setQuery] = useState('');
+
+    const filteredItems = useMemo(() => {
+        if (exercises.length === 0) return;
+        return exercises.filter(({ name, muscle_group, primary_muscle, balance }) => {
+            return balance.toLowerCase().includes(query.toLowerCase()) || name.toLowerCase().includes(query.toLowerCase()) || muscle_group.toLowerCase().includes(query.toLowerCase()) || primary_muscle.toLowerCase().includes(query.toLowerCase());
+        });
+    }, [exercises, query]);
 
     return (
         <>
             <section className="section-peak">
                 <h1 className="wr-title">Exercise Hub</h1>
+                <div className="wr-search-table">
+                    <label className="wr-label">Search
+                        <input
+                            className="wr-input"
+                            value={query}
+                            onChange={e => setQuery(e.target.value)}
+                            type="search" />
+                    </label>
+                </div>
                 <div className="table-container">
-                    {exercises ? <Table tName="ExerciseTable" editPath="/hub/exercises/edit/" tableData={exercises} keys={EXERCISE_KEY_LABELS} /> : <LoadingSpinner />}
+                    {filteredItems ? <Table tName="ExerciseTable" editPath="/hub/exercises/edit/" tableData={filteredItems} keys={EXERCISE_KEY_LABELS} /> : <LoadingSpinner />}
                 </div><br />
             </section>
             <AddExercise />
