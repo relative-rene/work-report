f4b1a6582d35aeb804f00b7945e11b0533b7f63f
relative-rene <rene.arellano415@gmail.com>
2024-05-30 11:35:21 -0700

    sort before setting

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -33,7 +33,7 @@ export const DataProvider = ({ children }) => {
         try {
             const res = await fetch(`${process.env.REACT_APP_SERVER}/api/profiles/${user_id}/read_sets`);
             const data = await res.json();
-            const formattedSets = data.map(({ _id, date_and_time, exercise_name, set_weight, total_reps, left_reps, right_reps }) => ({ _id, date_and_time: date_and_time ? formatTimeStampToUS(date_and_time) : "", exercise_name, set_weight, total_reps, left_reps, right_reps }));
+            const formattedSets = data.map(({ _id, date_and_time, exercise_name, set_weight, total_reps, left_reps, right_reps }) => ({ _id, date_and_time: date_and_time ? formatTimeStampToUS(date_and_time) : "", exercise_name, set_weight, total_reps, left_reps, right_reps })).sort((a, b) => new Date(a.date_and_time) - new Date(b.date_and_time));
             setSets(formattedSets);
         } catch (err) {
             console.error(err);
@@ -46,7 +46,7 @@ export const DataProvider = ({ children }) => {
             const data = await res.json()
             const formattedStats = data.map(({ _id, date, age, weight, body_fat, height, neck, chest, belly, butt, left_arm, right_arm, left_forearm, right_forearm, left_leg, right_leg }) => ({
                 _id, date: formatTimeStampToUS(date), age, weight, body_fat, height, neck, chest, belly, butt, left_arm, right_arm, left_forearm, right_forearm, left_leg, right_leg
-            }));
+            })).sort((a, b) => new Date(a.date) - new Date(b.date));
             setStats(formattedStats);
         } catch (err) {
             console.error(err);
