9cafdfd065872d8c14adaff36c8971b0f4915ae1
relative-rene <rene.arellano415@gmail.com>
2024-09-26 10:48:09 -0700

    fix aria voice reader redundancy

--- src/components/GuessBodyFat.jsx
+++ src/components/GuessBodyFat.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 
-import femaleBFComparison from "../assets/femaleBFComparison.jpeg";
-import maleBFComparison from "../assets/maleBFComparison.jpeg";
+import femaleBFComparison from "../assets/images/femaleBFComparison.jpeg";
+import maleBFComparison from "../assets/images/maleBFComparison.jpeg";
 
 const GuessBodyFat = () => {
-    const [showingMale, setShow]= useState(true);
+    const [showingMale, setShow] = useState(true);
     return (
         <>
             <h3>Which range do you most resemble?</h3>
-            <span className="moreOrLess" onClick={()=> setShow(!showingMale)}>{showingMale? "See Female":"See Male"}</span><br/>
-            {showingMale?<img width="100%" height="100%" src={maleBFComparison} alt="male comparison image" />:
-            <img width="100%" height="100%" src={femaleBFComparison} alt="female comparison image" />}
+            <span className="moreOrLess" onClick={() => setShow(!showingMale)}>{showingMale ? "See Female" : "See Male"}</span><br />
+            { showingMale ? 
+                <img width="100%" height="100%" src={maleBFComparison} alt="male comparison" /> :
+                <img width="100%" height="100%" src={femaleBFComparison} alt="female comparison" />
+            }
         </>
     )
 }
